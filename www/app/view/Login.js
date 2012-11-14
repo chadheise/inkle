d47fc25528fa11c29fb935561cd799ad7ae87bca
Chad Heise chad <Chad Heise chad@heise.org>
2012-11-13 22:42:32 -0800

    First redesign of login page

--- www/app/view/Login.js
+++ www/app/view/Login.js
@@ -13,13 +13,18 @@ Ext.define("inkle.view.Login", {
 		scrollable: false,
 		padding: "10px",
 		url: "http://127.0.0.1:8000/sencha/login/",
+		style: "background-image: -webkit-radial-gradient(center, circle farthest-corner, #288D42 0%, #1A492B 100%);",
 		
 		items: [
 			// Inkle logo
 		    {
 		        xtype: "panel",
-		        html: "<center><img src='resources/images/logo.png' style='padding-bottom: 20px;'/></center>"
+		        html: "<center><img src='resources/images/logoWhite.png' style='padding-top: 50px; padding-bottom: 60px; width: 90%;'/></center>"
 		    },
+		    { 
+		        xtype: "panel",
+		        html: "<center><img src='resources/images/fbLogin.jpeg' id='facebookLoginSubmitButton' style='width: 80%;'/></center>"
+		    }, 
 		    
 		    // Login form
 			{
@@ -49,17 +54,30 @@ Ext.define("inkle.view.Login", {
 				ui: "confirm"
 			},
 			
-			{
+			{ 
+		        xtype: "panel",
+		        html: "<center><img src='resources/images/line.png' style='width: 100%;'/></center>"
+		    },
+		    
+		    { 
+		        xtype: "panel",
+		        html: "<center><img src='resources/images/fbLogin.jpeg' style='width: 80%;'/></center>"
+		    },
+			{ 
+		        xtype: "panel",
+		        html: "<center><div style='color:#fff; padding-top: 50px;'><div>Sign Up  |  Take a Tour</div></div></center>"
+		    },
+			/*{
 			    xtype: "panel",
 			    html: "<center style='padding-top: 20px;'>OR</center>"
-			},
+			},*/
 			
-			{
+			/*{
 				xtype: "button",
 				itemId: "facebookLoginSubmitButton",
 				text: "Login with Facebook",
 				ui: "confirm"
-			}
+			}*/
 		
 			/*{
 			    xtype: "panel",
@@ -76,6 +94,7 @@ Ext.define("inkle.view.Login", {
 			{
 				delegate: "#facebookLoginSubmitButton",
             	event: "tap",
+            	element: "element",
             	fn: "onFacebookLoginSubmitButtonTap"
 			}
         ]
