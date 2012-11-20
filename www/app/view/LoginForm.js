2b87ab455eef698982856e7991b519fdad5fbbab
Jacob Wenger <wenger.jacob@gmail.com>
2012-11-19 22:32:59 -0800

    Updated login with email form.

--- www/app/view/LoginForm.js
+++ www/app/view/LoginForm.js
@@ -11,51 +11,81 @@ Ext.define("inkle.view.LoginForm", {
    	
    	config: {
 		scrollable: false,
-		style: "background-image: -webkit-radial-gradient(center, circle farthest-corner, #288D42 0%, #1A492B 100%)",
+		style: "background-image: -webkit-radial-gradient(center, circle farthest-corner, #EEEEEE 0%, #999999 100%)",
 		
 		url: "http://127.0.0.1:8000/sencha/login/",
 		
 		items: [
+		    // Top toolbar
+    		{
+    			xtype: "toolbar",
+    			id: "loginFormToolbar",
+    			title: "Inkle Login",
+                docked: "top",
+                ui: "customToolbar",
+                items: [
+                	{
+                		xtype: "button",
+                		id: "loginFormCancelButton",
+   		    			text: "Cancel",
+                		ui: "action"
+                	},
+                	{ xtype: "spacer" },
+                    {
+                		xtype: "button",
+                		id: "loginFormLoginButton",
+   		    			text: "Login",
+                		ui: "action"
+                	}
+                ]
+            },
+            
 		    // Login form
 			{
 				xtype: "fieldset",
+				id: "loginForm",
+				top: 0,
+				margin: 10,
+				width: "94%",
 				
 				items: [
 					{
 						xtype: "emailfield",
+						id: "loginFormEmail",
 						name: "email",
-						itemId: "loginEmail",
 						placeHolder: "Email"
 					},
 					{
 						xtype: "passwordfield",
+						id: "loginFormPassword",
 						name: "password",
-						itemId: "loginPassword",
 						placeHolder: "Password"
 					}
 				]
-			},
-			
-			// Submit button
-			{
-				xtype: "button",
-				itemId: "inkleLoginSubmitButton",
-				text: "Login",
-				ui: "confirm"
 			}
 		],
 
 		listeners: [
 			{
-            	delegate: "#inkleLoginSubmitButton",
+            	delegate: "#loginFormCancelButton",
             	event: "tap",
-            	fn: "onInkleLoginSubmitButtonTap"
+            	fn: "onLoginFormCancelButtonTap"
+        	},
+        	
+			{
+            	delegate: "#loginFormLoginButton",
+            	event: "tap",
+            	fn: "onLoginFormLoginButtonTap"
         	}
         ]
 	},
 	
 	// Event firings
-	onInkleLoginSubmitButtonTap: function () {
-        this.fireEvent("inkleLoginSubmitButtonTapped");
+	onLoginFormCancelButtonTap: function() {
+	    this.fireEvent("loginFormCancelButtonTapped");
+	},
+	
+	onLoginFormLoginButtonTap: function () {
+        this.fireEvent("loginFormLoginButtonTapped");
     }
 });
