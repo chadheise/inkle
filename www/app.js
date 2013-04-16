c942cfc6a5cf8e3129e527095f56850469fd67e9
Chad Heise chad <Chad Heise chad@heise.org>
2013-04-15 20:53:16 -0700

    Stable point but push notifications still don't work (commented out relevant code)

--- www/app.js
+++ www/app.js
@@ -67,6 +67,75 @@ Ext.application({
     baseUrl: "http://chads-macbook-pro.local:8000", //Used for testing from Chad's iPhone
     //baseUrl: "http://127.0.0.1:8000",
 
+    /*Push Notification functions */
+    myLog: document.getElementById("log"),
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+     bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('resume', this.onResume, false);
+    },
+    onResume: function() {
+        inkle.app.myLog.value="";
+        // Clear the badge number - if a new notification is received it will have a number set on it for the badge
+        inkle.app.setBadge(0);
+        inkle.app.getPending(); // Get pending since we were reopened and may have been launched from a push notification
+    },
+    onDeviceReady: function() {
+        inkle.app.register(); // Call to register device immediately every time since unique token can change (per Apple)
+        
+        // This will cause to fire when app is active already
+        document.addEventListener('push-notification', function(event) {
+            console.log('RECEIVED NOTIFICATION! Push-notification! ' + event);
+            inkle.app.myLog.value+=JSON.stringify(['\nPush notification received!', event]);
+            // Could pop an alert here if app is open and you still wanted to see your alert
+            //navigator.notification.alert("Received notification - fired Push Event " + JSON.stringify(['push-//notification!', event]));
+        });
+        document.removeEventListener('deviceready', this.deviceready, false);
+    },
+    setBadge: function(num) {
+        var pushNotification = window.plugins.pushNotification;
+        inkle.app.myLog.value+="Clear badge... \n";
+        pushNotification.setApplicationIconBadgeNumber(num);
+    },
+    receiveStatus: function() {
+        var pushNotification = window.plugins.pushNotification;
+        pushNotification.getRemoteNotificationStatus(function(status) {
+            inkle.app.myLog.value+=JSON.stringify(['Registration check - getRemoteNotificationStatus', status])+"\n";
+        });
+    },
+    getPending: function() {
+        var pushNotification = window.plugins.pushNotification;
+        pushNotification.getPendingNotifications(function(notifications) {
+            inkle.app.myLog.value+=JSON.stringify(['getPendingNotifications', notifications])+"\n";
+            console.log(JSON.stringify(['getPendingNotifications', notifications]));
+        });
+    },
+    register: function() {
+        var pushNotification = window.plugins.pushNotification;
+        pushNotification.registerDevice({alert:true, badge:true, sound:true}, function(status) {
+            inkle.app.myLog.value+=JSON.stringify(['registerDevice status: ', status])+"\n";
+            inkle.app.storeToken(status.deviceToken);
+        });
+    },
+    storeToken: function(token) {
+        console.log("Token is " + token);
+        var xmlhttp=new XMLHttpRequest();
+        xmlhttp.open("POST","http://127.0.0.1:8888",true);
+        xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        xmlhttp.send("token="+token+"&message=pushnotificationtester");
+        xmlhttp.onreadystatechange=function() {
+            if (xmlhttp.readyState==4) {
+                //a response now exists in the responseTest property.
+                console.log("Registration response: " + xmlhttp.responseText);
+                inkle.app.myLog.value+="Registration server returned: " + xmlhttp.responseText;
+            }
+        }
+    },
+    /******************************/
+
     /* Application launch */
     launch: function() {
         console.log("launching app");
@@ -90,6 +159,15 @@ Ext.application({
                         options.headers["X-CSRFToken"] = csrfToken;
                     }
                 }, this);
+
+                //Request push notification permissions
+                /*alert('inside app.js');
+                alert(window.plugins.pushNotification);
+                var pushNotification = window.plugins.pushNotification;
+                pushNotification.registerDevice({alert:true, badge:true, sound:true}, function(status) {
+                    inkle.app.myLog.value+=JSON.stringify(['registerDevice status: ', status])+"\n";
+                    inkle.app.storeToken(status.deviceToken);
+                });*/
 
                 // Determine if the user is logged in and show the appropriate page
                 Ext.Ajax.request({
@@ -129,7 +207,7 @@ Ext.application({
                 });
             }
         });
-
+        
         var _this = this;
         document.addEventListener("resume", function(){
             _this.resume();
@@ -140,7 +218,7 @@ Ext.application({
 
     resume: function() {
         console.log("resume");
-    }
+    },
 
     /*
     onUpdated: function() {
